dab9883f5fe4ff393612958080366d8fc7ffd951
seanl-adg <seanl@adguard.com>
2017-07-15 15:07:47 +0900

    Add more test, not use unnecessary constructor

--- index.js
+++ index.js
@@ -12,58 +12,51 @@ function AG_defineProperty ( path, descriptor, base ) {
         var prop = path.slice(0, pos);
         var nextPath = path.slice(pos + 1);
         if ( nextPath.length === 0 ) { return Object.defineProperty(obj, prop, descriptor); }
-        var desc = new Accessor(obj, prop, nextPath);
-        return (map[path] = Object.defineProperty(obj, prop, {
-            get: desc.get.bind(desc),
-            set: desc.set.bind(desc)
-        }))
+        return (map[path] = Object.defineProperty(obj, prop, makeAccessor(obj, prop, nextPath)));
     };
-    /** @constructor */
-    function Accessor(obj, prop, nextPath) {
-        this.obj = obj;
-        this.prop = prop;
-        this.desc = Object.getOwnPropertyDescriptor(obj, prop);
-        this.nextPath = nextPath;
-    }
-
-    Accessor.prototype.get = function() {
-        if ( !this.desc ) return undefined;
-        var val;
-        if ( this.desc.hasOwnProperty('value') ) { val = this.desc.value; }
-        else if ( !this.desc.get ) { return undefined; }
-        else { val = this.desc.get.call(this.obj); }
-        if ( !(val instanceof Object) ) { return val; }
-        var keys = getKey(val);
-        if ( keys.indexOf(this.nextPath) != -1 ) { return val; }
-        var l = keys.length;
-        var nextProp = this.nextPath.split('.', 1)[0];
-        while( l-- > 0 ) {
-            if ( keys[l].split('.', 1)[0] == nextProp ) {
-                console.warn('AG_defineProperty: unresolvable circular reference detected.');
-                return val;
+    var makeAccessor = function(obj, prop, nextPath) {
+        var desc = Object.getOwnPropertyDescriptor(obj, prop);
+        if ( desc ) {
+            delete desc.configurable;
+            delete desc.enumerable;
+        }
+        return {
+            get: function() {
+                if ( !desc ) return undefined;
+                var val;
+                if ( desc.hasOwnProperty('value') ) { val = desc.value; }
+                else if ( !desc.get ) { return undefined; }
+                else { val = desc.get.call(obj); }
+                if ( !(val instanceof Object) ) { return val; }
+                var keys = getKey(val);
+                if ( keys.indexOf(nextPath) != -1 ) { return val; }
+                var l = keys.length;
+                var nextProp = nextPath.split('.', 1)[0];
+                while( l-- > 0 ) {
+                    if ( keys[l].split('.', 1)[0] == nextProp ) {
+                        console.warn('AG_defineProperty: unresolvable circular reference detected.');
+                        return val;
+                    }
+                }
+                return override(val, nextPath);
+            },
+            set: function(incoming) {
+                if ( !desc ) {
+                    desc = { value: incoming, writable: true };
+                    return true;
+                }
+                if ( desc.hasOwnProperty('value') ) {
+                    if ( desc.writable ) {
+                        desc.value = incoming;
+                        return true;
+                    }
+                    else { return false; }
+                }
+                if ( !desc.set ) { return false; } // throw in strict mode
+                return desc.set.call(obj, incoming);
             }
-        }
-        return override(val, this.nextPath);
+        };
     };
-
-    Accessor.prototype.set = function(incoming) {
-        if ( !this.desc ) {
-            this.desc = {};
-            this.desc.value = incoming;
-            this.desc.writable = this.desc.configurable = this.desc.enumerable = true;
-            return true;
-        }
-        if ( this.desc.hasOwnProperty('value') ) {
-            if ( this.desc.writable ) {
-                this.desc.value = incoming;
-                return true;
-            }
-            else { return false; }
-        }
-        if ( !this.desc.set ) { return false; } // throw in strict mode
-        return this.desc.set.call(this.obj, incoming);
-    };
-
     path += ".";
     override(base || window, path);
 }
--- test.js
+++ test.js
@@ -5,7 +5,11 @@ suite('deep-override', function() {
     suite('Overriding non-existent object', function() {
         test('Overriding simple object literals', function() {
             var getCount = setCount = 0;
-            AG_defineProperty('test1.a.b', { get: function(){getCount++;return 1;}, set: function() {setCount++}, enumerable: true}, global);
+            AG_defineProperty('test1.a.b', {
+                get: () => { getCount++;return 1; },
+                set: () => { setCount++; },
+                enumerable: true
+            }, global);
             global.test1 = { c : 2 };
             test1.a = { d : 3 };
             test1 = { a : 4, c : 5 };
@@ -14,24 +18,91 @@ suite('deep-override', function() {
             test1.a = { b : { e : 6 }, c : 7 };
             assert.equal(test1.c, 5);
             assert.equal(setCount, 1);
+            assert.equal(getCount, 0);
             assert.equal(test1.a.b, 1);
             assert.equal(getCount, 1);
-
-            var json = JSON.parse(JSON.stringify(test1)); // ToDo: add ownKeys trap to make this work
+            var json = JSON.parse(JSON.stringify(test1));
             assert.equal(json.a.b, 1);
         });
     });
 
-    suite('Overriding existing object literal', function() {
-        test('value should be changed', function() {
+    suite('Overriding existing objects', function() {
+        test('New descriptor should be applied', function() {
             global.test2 = { a: { b: 1, c: 2 }, d: 3 };
             var value = test2.a.b;
-            AG_defineProperty('test2.a.b', {value: 4}, global);
+            AG_defineProperty('test2.a.b', { value: 4 }, global);
             var value2 = test2.a.b;
             assert.equal(value, 1);
             assert.equal(value2, 4);
         });
-    });
 
-    // ToDo: Overriding existing object defined with getters and setters
+        test('It should concatenate recursion', function() {
+            global.test3 = {};
+            test3.a = {};
+            test3.a.b = {};
+            test3.a.b.c = test3;
+            test3.a.b.c.d = {};
+            test3.a.b.c.d.e = {};
+
+            var getCount = setCount = 0;
+            AG_defineProperty('test3.a.b.c.d', {
+                get: function() {
+                    getCount++;
+                    return { f: null };
+                },
+                set: function() {
+                    setCount++;
+                }
+            }, global);
+
+            // Caveat: assert.equal(test3.d, null) does not hold in this case.
+            // Have to access test3.a.b.c.d once to make test3.d has a desired value.
+            assert(test3.a.b.c.d, null);
+            assert(test3.d, null);
+        })
+
+        test('It should not throw on pathological recursion', function() {
+            global.test4 = {};
+            test4.a = test4;
+            AG_defineProperty('test4.a.a', { value: null }, global);
+            assert(test4.a, test4);
+        });
+
+        test('It should override configurable object literal with getters and setters', function() {
+            global.test5 = {
+                a: {
+                    get b() {
+                        return {
+                            c: this.c++,
+                            d: 2
+                        };
+                    },
+                    set b(i) {
+                        this.c += i * 2;
+                    },
+                    c: 1
+                }
+            };
+            var getCount = setCount = 0;
+            assert.equal(test5.a.b.d, 2);
+            assert.equal(test5.a.b.c, 2);
+            AG_defineProperty('test5.a.b.d', {
+                get: () => {
+                    getCount++;
+                    return 3;
+                },
+                set: (i) => {
+                    setCount++;
+                }
+            }, global);
+            assert.equal(test5.a.b.d, 3);
+            assert.equal(getCount, 1);
+            assert.equal(test5.a.b.c, 4);
+            test5.a.b.d = 4;
+            assert.equal(test5.a.b.d, 3);
+            assert.equal(getCount, 2);
+            assert.equal(setCount, 1);
+        });
+
+    });
 });
